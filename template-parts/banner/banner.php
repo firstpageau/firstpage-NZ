6ed736f2dbbd1b48243e28f179d6eba7cdccee99
firstpageau <subscriptions@firstpage.com.au>
2021-12-17 11:50:17 +1100

    [UPDATE] tippy popup

--- template-parts/banner/banner.php
+++ template-parts/banner/banner.php
@@ -185,3 +185,52 @@
     <?php endif; ?>
     </div>
 <?php endif; ?>
+
+<div id="pop-up-form" style="display: none;">
+  <div class="embed-popup-form">
+    <div class="proposal-popup">
+        <!-- Free Proposal Form -->
+        <form class="fp-form" action="/thank-you" method="POST">
+            <h2>Enter your details</h2>
+            <div class="form-mid row">
+                <div class="col-12 mb-2">
+                    <input type="text" class="form-control" name="lead_name" placeholder="First Name*" required="" />
+                </div>
+                <div class="col-12 mb-2">
+                    <input type="tel" class="form-control" name="lead_phone" placeholder="Phone*" required="" />
+                </div>
+                <div class="col-12 mb-2">
+                    <input type="email" class="form-control" name="lead_email" placeholder="Email*" data-parsley-error-message="Please enter a valid email address" data-parsley-trigger="change" required="" />
+                </div>
+                <div class="col-12 mb-2">
+                    <input type="text" class="form-control" name="lead_website" placeholder="Website" data-parsley-website-check />
+                </div>
+
+                <div class="col-12 mt-2">
+                    <input type="hidden" name="lead_formname" value="" />
+                    <input type="hidden" name="lead_language" value="en" />
+                    <input type="hidden" name="lead_formtype" value="" />
+                    <button type="submit" class="form-control btn fp-btn fp-btn-orange fp-btn-shadow" >GET A FREE PROPOSAL</button>
+                </div>
+            </div>
+        </form>
+        <!-- End of Form -->
+    </div>
+</div>
+</div>
+
+<script>
+	tippy(".showpopup", {
+    html: ".embed-popup-form",
+    arrow: true,
+    trigger: "click",
+    interactive: true,
+    arrow: true,
+    placement: "bottom",
+    flip: false,
+    animation: "shift-toward",
+    inertia: true,
+    distance: 15,
+    arrowTransform: "scaleX(1.5)",
+  });
+</script>
