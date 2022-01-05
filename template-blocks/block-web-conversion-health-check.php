fd2d5b097bc00fb00dc547e875a9ee09c8da6696
firstpageau <subscriptions@firstpage.com.au>
2022-01-05 13:47:27 +1100

    [UPDATE] Generate revenue block

--- template-blocks/block-web-conversion-health-check.php
+++ template-blocks/block-web-conversion-health-check.php
@@ -27,8 +27,8 @@ if ( $background['image'] )
     <div class="block__web_conversion" style="padding: 4rem 0 !important;">
         <div class="section-letstalk section-orange section-letstalk-new">
             <div class="container text-center text-white">
-                <p class="mb-3 p-tag-h2-style"><strong>Claim your 100% free REVENUE</br class="d-none d-lg-block">GROWTH strategy session with an</br class="d-none d-lg-block">experienced digital strategist valued at</br class="d-none d-lg-block">$2000.</strong></p>
-                <p class="mb-4" style="font-size: 25px; font-weight:normal; line-height: 23px;">We outline bulletproof tactics that dramatically increase website traffic and revenue, even during tough economic times. </p>
+                <p class="mb-3 p-tag-h2-style"><strong>Claim your 100% free REVENUE</br class="d-none d-lg-block">GROWTH strategy session with an</br class="d-none d-lg-block">experienced digital strategist</br class="d-none d-lg-block"> valued at $2000.</strong></p>
+                <p class="mb-4" style="font-size: 25px; font-weight:normal; line-height: 35px;">We outline bulletproof tactics that dramatically</br class="d-none d-lg-block"> increase website traffic and revenue, even during tough</br class="d-none d-lg-block"> economic times. </p>
                 <p class="font-italic mb-4" style="font-size: 18px; font-weight:normal;">Hurry! Limited spots available.</p>
                 <a href="/free-growth-strategy" class="btn fp-btn fp-btn-white fp-btn-shadow fp-session free-session-btn">Get my free revenue growth session</a>
             </div>
