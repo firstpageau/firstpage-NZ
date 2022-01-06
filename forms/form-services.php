cbe72643bfc6c5b8127696129b2b91d7be96aeb7
firstpageau <subscriptions@firstpage.com.au>
2022-01-06 15:07:00 +1100

    []

--- forms/form-services.php
+++ forms/form-services.php
@@ -6,7 +6,7 @@
 
 <div class="quote-form quote-form-big">
 	<!-- Quote Form -->
-	<form class="fp-form" action="/thankyou" method="POST">
+	<form class="fp-form" action="/thank-you" method="POST">
 		<div class="form-step-1 form-step">
 				<div class="input-group">
 						<div class="select-container form-control">
