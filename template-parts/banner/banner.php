0ac8877aa78460c6b4357ffc43f46c4d2def2003
firstpageau <subscriptions@firstpage.com.au>
2022-01-12 16:53:23 +1100

    [UPDATE]

--- template-parts/banner/banner.php
+++ template-parts/banner/banner.php
@@ -49,7 +49,7 @@
         </div>
         <?php if(get_sub_field('name_tag')): ?>
             <div class="name-tag">
-                <h3><b><?php the_sub_field('name_tag'); ?></b>
+                <h3 class="text-left"><b><?php the_sub_field('name_tag'); ?></b>
                 <br>
                 <?php the_sub_field('designation'); ?></h3>
             </div>
