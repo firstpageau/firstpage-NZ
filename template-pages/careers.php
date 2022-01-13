28c1fa3329b956269fb7abc952ba2969f515406f
firstpageau <subscriptions@firstpage.com.au>
2022-01-13 18:22:42 +1100

    [UPDATE] Career page banner button

--- template-pages/careers.php
+++ template-pages/careers.php
@@ -13,7 +13,7 @@ get_header(); ?>
             <div class="col-lg-5 text-center text-lg-left">
                 <h1>Interested in joining a company with an unbeatable culture?</h1>
                 <p>Join a company that doesn’t take shortcuts or treat you like a number. We help each other win and that’s why we are the fastest growing digital marketing agency in APAC.</p>
-                <a href="#vacancies" class="btn fp-btn fp-btn-orange show-popup-form-3">VIEW OUR OPPORTUNITIES</a>
+                <a href="#vacancies" class="btn fp-btn fp-btn-orange">VIEW OUR OPPORTUNITIES</a>
             </div>
         </div>
     </div>
